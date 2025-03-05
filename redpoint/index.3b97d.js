e10128ba971f999018dcdd92c2e65ccc4bd9b24b
kaxifa <1813408689@qq.com>
2025-03-05 17:43:21 +0800

    更新红点

--- redpoint/index.3b97d.js
+++ redpoint/index.3b97d.js
@@ -1,4 +1,4 @@
-System.register(["./application.e747d.js"], function (_export, _context) {
+System.register(["./application.ea71e.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;

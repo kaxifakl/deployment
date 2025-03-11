00664c8f3803048dd08e783324af02a2cdb2eea1
kaxifa <1813408689@qq.com>
2025-03-11 10:06:50 +0800

    更新案例10

--- redpoint/index.cffee.js
+++ redpoint/index.cffee.js
@@ -1,4 +1,4 @@
-System.register(["./application.ea71e.js"], function (_export, _context) {
+System.register(["./application.724fc.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
